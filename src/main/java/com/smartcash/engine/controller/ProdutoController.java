bcb18eb3217d70d336f26daffe2e135cf45f9237
Angelina <angelina.barboza@hotmail.com.br>
2022-05-02 17:04:03 -0300

    Adicionando crud de produto

---
+++ src/main/java/com/smartcash/engine/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package com.smartcash.engine.controller;
+
+import com.smartcash.engine.models.domain.Produto;
+import com.smartcash.engine.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    @Autowired
+    ProdutoService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Produto produto) {
+        service.create(produto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        List<Produto> produtos = service.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id) {
+        Optional<Produto> produto = service.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(produto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@PathVariable Long id, @RequestBody Produto produto) {
+        service.update(id, produto);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+}
