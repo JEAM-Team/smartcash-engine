5d31d78c662a4b69bff18f366a5dc53d02b4921a
Angelina <angelina.barboza@hotmail.com.br>
2022-05-10 08:29:23 -0300

    Metodo calcula total

--- src/main/java/com/smartcash/engine/services/NotaService.java
+++ src/main/java/com/smartcash/engine/services/NotaService.java
@@ -2,7 +2,9 @@ package com.smartcash.engine.services;
 
 import com.smartcash.engine.models.domain.Atividade;
 import com.smartcash.engine.models.domain.Nota;
+import com.smartcash.engine.models.dtos.CalculaResultadoDto;
 import com.smartcash.engine.models.dtos.NotaDto;
+import com.smartcash.engine.models.enums.TipoNota;
 import com.smartcash.engine.repository.NotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -57,5 +59,13 @@ public class NotaService {
 
     public List<Nota> findByContaId(Long contaId) {
         return repository.findByContaId(contaId);
+    }
+
+    public CalculaResultadoDto calculaTotal() {
+        Double totalPagamento = repository.countTotal(TipoNota.PAGAMENTO);
+        Double totalRecebimento = repository.countTotal(TipoNota.RECEBIMENTO);
+
+        return CalculaResultadoDto.builder().totalPagamento(totalPagamento).totalRecebimento(totalRecebimento).build();
+
     }
 }
